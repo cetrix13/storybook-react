8365a3c69ddbc717e3c546cff0069d016d342e30
Alexandr Dadykin <DadykinAV@yandex.ru>
2019-01-11 13:17:35 +0300

    Move variables inside component

--- src/components/Button/RadioButton.js
+++ src/components/Button/RadioButton.js
@@ -2,23 +2,27 @@ import React from 'react';
 import './css/default-button.css';
 import './css/radio-button.css';
 
-const options = ["Yesterday", "Tomorrow", "Today", "In a week"];
-const classes = ["Button"];
 class RadioButton extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             activeBtn: 0
         };
+        this.classes = ["Button"];
+        this.options = ["Yesterday", "Tomorrow", "Today", "In a week"];
     }
 
     render() {
         return(
         <div class="buttons">
         {
-            options.map((item, index) => {
-                (this.state.activeBtn === index) ? classes.push('is-active') : classes.splice(1,1);
-                return (<span class={classes.join(" ")} onClick={()=> {this.setState({activeBtn: index})}}>{item}</span>);
+            this.options.map((item, index) => {
+                (this.state.activeBtn === index) ? this.classes.push(['is-active']) : this.classes.splice(1,2);
+                return <span
+                    class={ this.classes.join(" ") }
+                    onClick={ ()=> {this.setState({activeBtn: index}) }}>
+                    { item }
+                </span>;
             })
         }
         </div>
